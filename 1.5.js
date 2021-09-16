035927e131552f4da8d697e51ce85c2a726880ce
ThomasBackers <thomasbackers@gmail.com>
2021-09-16 14:46:52 +0200

    finished 1.5.js

--- 1.5.js
+++ 1.5.js
@@ -2,3 +2,6 @@ const readlineSync = require("readline-sync");
 console.log("/!\\ WARNING: Separate all the inputs by a white space");
 let userInput = readlineSync.question("Can you give me two decimal numbers? ");
 userInputs = userInput.split(" ");
+let firstNumber = new Number(userInputs[0][0]);
+let secondNumber = new Number(userInputs[1]);
+console.log(firstNumber * secondNumber);
