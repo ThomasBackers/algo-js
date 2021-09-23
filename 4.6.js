68f20433c2f18e14ee1a465e70aab11a224c3d4c
ThomasBackers <thomasbackers@gmail.com>
2021-09-23 16:11:57 +0200

    4.6.js done damn it git add *git add *

---
+++ 4.6.js
@@ -0,0 +1,32 @@
+const readlineSync = require("readline-sync");
+const userInput = readlineSync.question("the factorial of which number do you want? ");
+//non-recursive
+const factorial = a => {
+    let product = 1;
+    if (a < 0) {
+        return "Sorry, but it's a factorial algorithm =/";
+    }
+    if (a == 0) {
+        return product;
+    }
+    for (let i = 1; i <= a; i++) {
+        product *= i;
+    }
+    return product
+}
+console.log(factorial(userInput));
+//recursive
+const recursiveFactorial = a => {
+    if (a < 0) {
+        return "Sorry, but it's a factorial algorithm =/";
+    }
+    if (a == 0) {
+        return 1;
+    }
+    if (a == 1) {
+        return a;
+    } else {
+        return a  * recursiveFactorial(a - 1);
+    }
+}
+console.log(recursiveFactorial(userInput));
