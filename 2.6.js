a7a22be4927d5ba463ccd35911ad484f52546e39
ThomasBackers <thomasbackers@gmail.com>
2021-09-23 12:09:03 +0200

    2.6.js done

---
+++ 2.6.js
@@ -0,0 +1,27 @@
+const readlineSync = require("readline-sync");
+const userInput = readlineSync.question("Can you give me a number between 1 and 7? ");
+switch (userInput) {
+    case "1":
+        console.log("Monday");
+        break;
+    case "2":
+        console.log("Tuesday");
+        break;
+    case "3":
+        console.log("Wednesday");
+        break;
+    case "4":
+        console.log("Thursday");
+        break;
+    case "5":
+        console.log("Friday");
+        break;
+    case "6":
+        console.log("Saturday");
+        break;
+    case "7":
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Not between 1 and 7 :(");
+}
