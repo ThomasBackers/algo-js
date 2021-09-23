382a02827ff6ef3fd6d48337183d6c508ef71512
ThomasBackers <thomasbackers@gmail.com>
2021-09-23 15:17:22 +0200

    4.3.js done

---
+++ 4.3.js
@@ -0,0 +1,14 @@
+const readlineSync = require("readline-sync");
+const rand10 = () => {
+    /**return a random integer from 0 to 10 included*/
+    return Math.ceil(Math.random() * 10);
+}
+const multirand = (n) => {
+    const array = [];
+    for (let i = 0; i < n; i++) {
+        array.push(rand10());
+    }
+    return array;
+}
+const userInput = readlineSync.question("How many random numbers do you want? ");
+console.log(multirand(userInput));
