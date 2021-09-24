157ee544035f193607c2fe535b3ffda28aa86525
ThomasBackers <thomasbackers@gmail.com>
2021-09-24 09:21:21 +0200

    5.1.js done

--- 5.1.js
+++ 5.1.js
@@ -1,1 +1,14 @@
-const askTvSerie() = () => {}
+const readlineSync = require("readline-sync");
+const askTvSerie = () => {
+    console.log("/!\\ WARNING: Separate all the inputs by a comma + a white space");
+    const userInput = readlineSync.question("What's the name,\nthe year of production\nand the cast members of your serie? ");
+    const userInputs = userInput.split(", ");
+    const name = userInputs.shift(), yearOfProduction = userInputs.shift();
+    const tvSerie = {
+        name: name,
+        yearOfProduction: yearOfProduction,
+        cast: userInputs
+    };
+    console.log(tvSerie);
+}
+askTvSerie();
