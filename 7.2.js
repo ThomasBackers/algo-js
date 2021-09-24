b3cf62f5b319e06f87227827afbb371b09ab3882
ThomasBackers <thomasbackers@gmail.com>
2021-09-24 16:03:01 +0200

    7.2.js updated

--- 7.2.js
+++ 7.2.js
@@ -1,13 +1,16 @@
 const readlineSync = require("readline-sync");
 userInput = parseInt(readlineSync.question("How many Fibonacci numbers do you want ? "));
-let a = 0, b = 1, c;
+let a = 0,
+    b = 1,
+    c;
 if (userInput === 1) {
     console.log(a);
-} if (userInput === 2) {
+}
+if (userInput === 2) {
     console.log(b);
 } else {
-    console.log(`${a}\n${b}`)
-    for (let i = 0; i < userInput-2; i++) {
+    console.log(`${a}\n${b}`);
+    for (let i = 0; i < userInput - 2; i++) {
         c = a + b;
         console.log(c);
         a = b;
