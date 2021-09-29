8495a3144bb471baf61bb7d6fa0ec87cf0af917a
ThomasBackers <90030136+ThomasBackers@users.noreply.github.com>
2021-09-29 10:34:25 +0200

    Update 4.3.js

--- 4.3.js
+++ 4.3.js
@@ -1,10 +1,10 @@
 const readlineSync = require("readline-sync");
 const rand10 = () => {
-    /**return a random integer from 0 to 10 included*/
+    /**return a random integer from 1 to 10 included*/
     return Math.ceil(Math.random() * 10);
 };
 const multirand = n => {
-    /**return a array of n random integers from 0 to 10 included*/
+    /**return a array of n random integers from 1 to 10 included*/
     const array = [];
     for (let i = 0; i < n; i++) {
         array.push(rand10());
