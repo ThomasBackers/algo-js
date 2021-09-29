f0e476bd9de297704e7ffc7e1312d05e89a7892d
ThomasBackers <90030136+ThomasBackers@users.noreply.github.com>
2021-09-29 10:38:21 +0200

    Update 4.4.js

--- 4.4.js
+++ 4.4.js
@@ -27,11 +27,11 @@ const max = arr => {
     return max;
 };
 const rand10 = () => {
-    /**return a random integer from 0 to 10 included*/
+    /**return a random integer from 1 to 10 included*/
     return Math.ceil(Math.random() * 10);
 };
 const multirand = n => {
-    /**return a array of n random integers from 0 to 10 included*/
+    /**return a array of n random integers from 1 to 10 included*/
     const array = [];
     for (let i = 0; i < n; i++) {
         array.push(rand10());
